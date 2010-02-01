9c9a687b4040dba3b4143ca15b6aae49ace10284
Sean O'Connor <sean@seanoc.com>
2010-02-01 09:33:32 -0500

    Added a model for dealing with existing pad info in the etherpad db.

--- pyconpads/pads/models.py
+++ pyconpads/pads/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Pad(models.Model):
+    id = models.CharField(max_length=128, primary_key=True, db_column='ID')
+    json = models.TextField(db_column='JSON')
+    
+    class Meta:
+        db_table = 'PAD_APOOL'
+        managed = False
+        
+    def __unicode__(self):
+        return self.id
